2e9ac4a3ab381c06027f8bae268bcff142fc6b87
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2023-05-10 20:16:29 -0500

    script.js

--- SCANWEB/script.js
+++ SCANWEB/script.js
@@ -1,40 +1,71 @@
 function extractData() {
-    var urlInput = document.getElementById('url-input').value.trim();
-    var linksList = document.getElementById('links-list');
-    var subdomainsList = document.getElementById('subdomains-list');
-  
-    // Reiniciar resultados anteriores
-    linksList.innerHTML = '';
-    subdomainsList.innerHTML = '';
-  
-    // Expresión regular para extraer enlaces
-    var linkRegex = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/gi;
-    var matches;
-  
-    while ((matches = linkRegex.exec(urlInput)) !== null) {
-      var link = matches[2].trim();
-      var listItem = document.createElement('li');
-      listItem.textContent = link;
-      linksList.appendChild(listItem);
-    }
-  
-    // Expresión regular para extraer subdominios
-    var subdomainRegex = /^(?:https?:\/\/)?([^\/\s]+)\./gi;
-    var subdomains = new Set();
-  
-    while ((matches = subdomainRegex.exec(urlInput)) !== null) {
-      var subdomain = matches[1].trim().toLowerCase();
-      subdomains.add(subdomain);
-    }
-  
-    subdomains.forEach(function(subdomain) {
-      var listItem = document.createElement('li');
-      listItem.textContent = subdomain;
-      subdomainsList.appendChild(listItem);
-    });
-  
-    // Mostrar resultados
-    var resultSection = document.getElementById('result-section');
-    resultSection.style.display = 'block';
+  var urlInput = document.getElementById("url-input");
+  var url = urlInput.value.trim();
+
+  // Validar que la URL tenga el formato correcto
+  var urlPattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\/.*)?$/;
+  if (!urlPattern.test(url)) {
+    alert("Ingresa una URL válida en el formato http://www.ejemplo.com");
+    return;
   }
-  
+
+  // Realizar una solicitud HTTP para obtener el HTML de la página
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      var html = this.responseText;
+
+      // Extraer URLs y subdominios
+      var urls = extractUrls(html);
+      var subdomains = extractSubdomains(urls);
+
+      // Mostrar resultados
+      displayResults(urls, subdomains);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function extractUrls(html) {
+  var urlRegex = /((https?|ftp):\/\/[^\s/$.?#].[^\s]*)/g;
+  var urls = html.match(urlRegex) || [];
+  return urls;
+}
+
+function extractSubdomains(urls) {
+  var subdomains = [];
+  var subdomainRegex = /^(https?:\/\/)?([^\/]+)\./i;
+
+  for (var i = 0; i < urls.length; i++) {
+    var match = urls[i].match(subdomainRegex);
+    if (match && match[2]) {
+      subdomains.push(match[2]);
+    }
+  }
+
+  return subdomains;
+}
+
+function displayResults(urls, subdomains) {
+  var linksList = document.getElementById("links-list");
+  var subdomainsList = document.getElementById("subdomains-list");
+
+  // Limpiar resultados anteriores
+  linksList.innerHTML = "";
+  subdomainsList.innerHTML = "";
+
+  // Mostrar URLs
+  for (var i = 0; i < urls.length; i++) {
+    var linkItem = document.createElement("li");
+    linkItem.textContent = urls[i];
+    linksList.appendChild(linkItem);
+  }
+
+  // Mostrar subdominios
+  for (var j = 0; j < subdomains.length; j++) {
+    var subdomainItem = document.createElement("li");
+    subdomainItem.textContent = subdomains[j];
+    subdomainsList.appendChild(subdomainItem);
+  }
+}
